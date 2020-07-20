7628f0b41665ca821ea32651eeb1cc6be87966b5
osx user <gili.shemesh@gmail.com>
2020-07-20 11:01:23 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   // options...
   publicPath: "/desktop-search/",
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = 'Bookmark Manager'
+      return args
+    })
+  },
 }
