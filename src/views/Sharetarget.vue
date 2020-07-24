818223c27c1b726986f011f0268251e862825e77
osx user <gili.shemesh@gmail.com>
2020-07-25 02:46:58 +0300

    commit

--- src/views/Sharetarget.vue
+++ src/views/Sharetarget.vue
@@ -14,8 +14,22 @@ export default {
     const parsedUrl = new URL(window.location);
     // searchParams.get() will properly handle decoding the values.
     const title =  parsedUrl.searchParams.get('title') ? parsedUrl.searchParams.get('title') : "";
-    const text =  parsedUrl.searchParams.get('text') ? parsedUrl.searchParams.get('text') : "";
-    const url = parsedUrl.searchParams.get('url') ? parsedUrl.searchParams.get('url') : "";
+    let text =  parsedUrl.searchParams.get('text') ? parsedUrl.searchParams.get('text') : "";
+    let url = parsedUrl.searchParams.get('url') ? parsedUrl.searchParams.get('url') : "";
+
+    const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    const isTextUrl =  !!pattern.test(text);
+
+    if ( isTextUrl && url ==="") {
+      url = text;
+      text = ""
+    }
+
 
     const r = {title: title, url: url, description: text, keywords: "", hostname: ""};
 
