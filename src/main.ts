68284c4e00cfcc785060b3f62b5e10d7e1919e53
osx user <gili.shemesh@gmail.com>
2021-01-20 17:26:38 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import "./compositionPlugin";
 
 Vue.config.productionTip = false
 
